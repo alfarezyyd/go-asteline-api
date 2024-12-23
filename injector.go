ef8837d3829a8e24ed11cd78c24e3bb896f70329
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-23 19:14:44 +0700

    feat: campaign controller create via DI

--- injector.go
+++ injector.go
@@ -7,13 +7,12 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/google/wire"
 	"github.com/spf13/viper"
-	"go-asteline-api/config"
+	"go-asteline-api/campaign"
 	"go-asteline-api/user"
 	"gorm.io/gorm"
 )
 
 var userFeatureSet = wire.NewSet(
-	config.NewDatabaseConnection,
 	user.NewRepository,
 	wire.Bind(new(user.Repository), new(*user.RepositoryImpl)),
 	user.NewService,
@@ -22,7 +21,21 @@ var userFeatureSet = wire.NewSet(
 	wire.Bind(new(user.Controller), new(*user.Handler)),
 )
 
+var campaignFeatureSet = wire.NewSet(
+	campaign.NewRepository,
+	wire.Bind(new(campaign.Repository), new(*campaign.RepositoryImpl)),
+	campaign.NewService,
+	wire.Bind(new(campaign.Service), new(*campaign.ServiceImpl)),
+	campaign.NewHandler,
+	wire.Bind(new(campaign.Controller), new(*campaign.Handler)),
+)
+
 func InitializeUserController(gormConnection *gorm.DB, validatorInstance *validator.Validate, viperConfig *viper.Viper) user.Controller {
 	wire.Build(userFeatureSet)
+	return nil
+}
+
+func InitializeCampaignController(gormConnection *gorm.DB, validatorInstance *validator.Validate) campaign.Controller {
+	wire.Build(campaignFeatureSet)
 	return nil
 }
--- wire_gen.go
+++ wire_gen.go
@@ -10,7 +10,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/google/wire"
 	"github.com/spf13/viper"
-	"go-asteline-api/config"
+	"go-asteline-api/campaign"
 	"go-asteline-api/user"
 	"gorm.io/gorm"
 )
@@ -24,6 +24,15 @@ func InitializeUserController(gormConnection *gorm.DB, validatorInstance *valida
 	return handler
 }
 
+func InitializeCampaignController(gormConnection *gorm.DB, validatorInstance *validator.Validate) campaign.Controller {
+	repositoryImpl := campaign.NewRepository()
+	serviceImpl := campaign.NewService(gormConnection, repositoryImpl, validatorInstance)
+	handler := campaign.NewHandler(serviceImpl)
+	return handler
+}
+
 // injector.go:
 
-var userFeatureSet = wire.NewSet(config.NewDatabaseConnection, user.NewRepository, wire.Bind(new(user.Repository), new(*user.RepositoryImpl)), user.NewService, wire.Bind(new(user.Service), new(*user.ServiceImpl)), user.NewHandler, wire.Bind(new(user.Controller), new(*user.Handler)))
+var userFeatureSet = wire.NewSet(user.NewRepository, wire.Bind(new(user.Repository), new(*user.RepositoryImpl)), user.NewService, wire.Bind(new(user.Service), new(*user.ServiceImpl)), user.NewHandler, wire.Bind(new(user.Controller), new(*user.Handler)))
+
+var campaignFeatureSet = wire.NewSet(campaign.NewRepository, wire.Bind(new(campaign.Repository), new(*campaign.RepositoryImpl)), campaign.NewService, wire.Bind(new(campaign.Service), new(*campaign.ServiceImpl)), campaign.NewHandler, wire.Bind(new(campaign.Controller), new(*campaign.Handler)))
