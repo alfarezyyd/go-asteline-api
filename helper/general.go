270c176a88e3208c3c740392cdf7f52082dc35f7
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-23 12:47:25 +0700

    feat: better error check

--- helper/general.go
+++ helper/general.go
@@ -4,10 +4,12 @@ import (
 	"gorm.io/gorm"
 )
 
-func CheckErrorOperation(indicatedError error, errorForwarder func()) {
+func CheckErrorOperation(indicatedError error, errorForwarder func()) bool {
 	if indicatedError != nil {
 		errorForwarder()
+		return true
 	}
+	return false
 }
 
 func TransactionOperation(runningTransaction *gorm.DB) {
