9cc5f225f30c90ad8bba5e69080a030a7e7962af
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-23 12:47:00 +0700

    feat: user mapper

---
+++ mapper/user_mapper.go
@@ -0,0 +1,37 @@
+package mapper
+
+import (
+	"fmt"
+	"github.com/go-viper/mapstructure/v2"
+	"go-asteline-api/model"
+	"go-asteline-api/user/dto"
+	"time"
+)
+
+func MapUserRegisterDtoIntoUserModel(userRegisterDto dto.UserRegisterDto) (*model.User, error) {
+	// Konversi struct dto ke map
+	var userMap map[string]interface{}
+	err := mapstructure.Decode(userRegisterDto, &userMap)
+	if err != nil {
+		return nil, err
+	}
+
+	// Custom handling untuk field yang membutuhkan konversi (misalnya BirthDate)
+	birthDate, err := time.Parse("2006-01-02", userRegisterDto.BirthDate)
+	if err != nil {
+		return nil, err
+	}
+
+	// Tambahkan konversi manual
+	userMap["BirthDate"] = birthDate
+
+	// Decode kembali ke struct User
+	var user model.User
+	fmt.Println(userMap)
+	err = mapstructure.Decode(userMap, &user)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(user)
+	return &user, nil
+}
