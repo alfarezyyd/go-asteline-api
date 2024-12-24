10b41e3f4a63aa7e6e8f039a1849dadaa9170828
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-24 14:31:36 +0700

    feat: add response when rollback

--- helper/general.go
+++ helper/general.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 func CheckErrorOperation(indicatedError error, ginContext *gin.Context, httpStatus int) bool {
@@ -13,10 +14,11 @@ func CheckErrorOperation(indicatedError error, ginContext *gin.Context, httpStat
 	return false
 }
 
-func TransactionOperation(runningTransaction *gorm.DB) {
+func TransactionOperation(runningTransaction *gorm.DB, ginContext *gin.Context) {
 	occurredError := recover()
 	if occurredError != nil {
 		runningTransaction.Rollback()
+		ginContext.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": runningTransaction.Error.Error()})
 	} else {
 		runningTransaction.Commit()
 	}
