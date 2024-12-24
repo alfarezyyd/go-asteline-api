b76632f7370b760f3940305c48a7756f606a5424
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-24 14:31:20 +0700

    feat: change data type

--- model/campaign.go
+++ model/campaign.go
@@ -3,16 +3,17 @@ package model
 import "time"
 
 type Campaign struct {
-	ID            uint64    `gorm:"primary_key;auto_increment"`
+	ID            uint64    `gorm:"primary_key;auto_increment;->:"`
 	Title         string    `gorm:"column:title;"`
 	Description   string    `gorm:"column:description"`
-	GoalAmount    uint64    `gorm:"column:goal_amount"`
+	GoalAmount    float64   `gorm:"column:goal_amount"`
 	CurrentAmount uint64    `gorm:"column:current_amount"`
 	StartDate     string    `gorm:"column:start_date"`
 	EndDate       string    `gorm:"column:end_date"`
 	ImageUrl      string    `gorm:"column:image_url"`
 	status        string    `gorm:"column:status"`
 	UserId        uint64    `gorm:"column:user_id"`
+	User          User      `gorm:"foreignKey:user_id;references:id"`
 	CreatedAt     time.Time `gorm:"column:created_at;autoCreateTime" mapstructure:"created_at"`
 	UpdatedAt     time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime" mapstructure:"updated_at"`
 }
